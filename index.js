aa281b1564defc3201a6986fdf8678ad595dfa35
Philip Orvis <orvisuals@gmail.com>
2020-03-27 19:48:34 -0500

    added animation, linked example gallery

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
-function homeScreen() {
-    $('.navbar').on('click', '.home', function() {
+
+function show(sectionButton, sectionSelector) {
+    $('.navbar').on('click', sectionButton, function() {
         event.preventDefault();
-        console.log('home button clicked');
+        console.log(sectionButton + ' button clicked');
         $('.content').hide();
-        $('.homePage').fadeIn();
+        $(sectionSelector).fadeIn();
+        window.scrollTo(0, 0);
     })
-
 }
 
-function bioScreen() {
-    $('.navbar').on('click', '.bio', function() {
-        event.preventDefault();
-        console.log('bio button clicked');
-        $('.content').hide();
-        $('.bioPage').fadeIn();
-    })
-
-}
-
-function skillsScreen() {
-    $('.navbar').on('click', '.skills', function() {
-        event.preventDefault();
-        console.log('skills button clicked');
-        $('.content').hide();
-        $('.skillsPage').fadeIn();
-    })
-
-}
-
-function galleryScreen() {
-    $('.navbar').on('click', '.gallery', function() {
-        event.preventDefault();
-        console.log('gallery button clicked');
-        $('.content').hide();
-        $('.galleryPage').fadeIn();
-    })
-
-}
-
-function contactInfoScreen() {
-    $('.navbar').on('click', '.contactInfo', function() {
-        event.preventDefault();
-        console.log('contactInfo button clicked');
-        $('.content').hide();
-        $('.contactInfoPage').fadeIn();
-    })
-
-}
-
-function portfolio() {
-    homeScreen();
-    bioScreen();
-    skillsScreen();
-    galleryScreen();
-    contactInfoScreen();
-}
-
-portfolio();
-
+show('.home', '.homePage')
+show('.bio', '.bioPage')
+show('.skills', '.skillsPage')
+show('.gallery', '.galleryPage')
+show('.contactInfo', '.contactInfoPage')
